386738232edf7365c0776b3db5dc0064bb0cc3e1
Alena Svetkina <alsvetkina@gmail.com>
2022-06-23 15:16:06 +0200

    use route and install react-router-dom

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return (
-        <div className={classes.content}>
+        <div>
             <div>
                 <img className='big-image' src='https://images.unsplash.com/photo-1610878180933-123728745d22?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FuYWRhJTIwbmF0dXJlfGVufDB8fDB8fA%3D%3D&w=1000&q=80'></img>
             </div>
