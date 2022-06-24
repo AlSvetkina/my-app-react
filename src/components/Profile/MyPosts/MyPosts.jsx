24691308d0654bdbb4fbec064b367092858cb0b9
Alena Svetkina <alsvetkina@gmail.com>
2022-06-24 20:48:17 +0200

    refactoring

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,16 +4,20 @@ import Post from './Post/Post';
 
 const MyPosts = (props) => {
     return (
-        <div>
-            { props.hey }
+        <div className={classes.postBlock}>
+            <h3>My posts</h3>
             <div>
-                <textarea></textarea>
-                <button>Add post</button>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Add post</button>
+                </div>
             </div>
             <div className={classes.posts}>
-            <Post message='Hi, how are you?' likeCount='23' />
-            <Post message='it is my first post' likeCount='56' />
-            {/* вызываем функцию дважды с атрибутом message и благодаря props из Post.jsx данные подставляются в функцию */}
+                <Post message='Hi, how are you?' likeCount='23' />
+                <Post message='it is my first post' likeCount='56' />
+                {/* вызываем функцию дважды с атрибутом message и благодаря props из Post.jsx данные подставляются в функцию */}
             </div>
         </div>
     );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import classes from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = () => {
     return (
         <div>
-            <div>
-                <img className='big-image' src='https://images.unsplash.com/photo-1610878180933-123728745d22?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FuYWRhJTIwbmF0dXJlfGVufDB8fDB8fA%3D%3D&w=1000&q=80'></img>
-            </div>
-            <div>
-                ava
-            </div>
-            <MyPosts hey='yo' />
+            <ProfileInfo />
+            <MyPosts />
         </div>
     ) ;
 }
