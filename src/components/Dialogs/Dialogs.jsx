bd9a175d79c67dd22ea3e2c8e9d4c74a7a4e9875
Alena Svetkina <alsvetkina@gmail.com>
2022-06-27 20:23:11 +0200

    refactoring with variables

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -19,19 +19,40 @@ const Message = (props) => {
     );
 }
 
+let dialogsData = [
+    {id: '1', name: 'Dima'}, 
+    {id: '2', name: 'Alena'}, 
+    {id: '3', name: 'Mikl'}, 
+    {id: '4', name: 'John'}
+]
+
+let dialogsElements = [ 
+    <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>,
+    <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>,
+    <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>,
+    <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+];
+
+let messagesData = [
+    {id: '1', message: 'Hi'}, 
+    {id: '2', message: 'How is your day?'}, 
+    {id: '3', message: 'How is your vacation?'},
+    {id: '4', message: 'Have a nice day'}
+]
+
 const Dialogs = (props) => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <DialogItem name="Dima" id="1"/>
-                <DialogItem name="Alena" id="2"/>
-                <DialogItem name="Mikl" id="3"/>
-                <DialogItem name="John" id="4"/>
+
+                { dialogsElements }
+
             </div>
             <div className={classes.messages}>
-                <Message text="Hi"/>
-                <Message text="How is your day?"/>
-                <Message text="ow is your vacation?<"/>
+                {/* <Message text={messagesData[0].message}/>
+                <Message text={messagesData[1].message}/>
+                <Message text={messagesData[2].message}/>
+                <Message text={messagesData[3].message}/> */}
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,12 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
+
+    let postData = [
+        {id: '1', message: 'Hi, how are you?', likeCount: '12'}, 
+        {id: '2', message: 'It is my first post', likeCount: '15'} 
+    ]
+
     return (
         <div className={classes.postBlock}>
             <h3>My posts</h3>
@@ -15,8 +21,8 @@ const MyPosts = (props) => {
                 </div>
             </div>
             <div className={classes.posts}>
-                <Post message='Hi, how are you?' likeCount='23' />
-                <Post message='it is my first post' likeCount='56' />
+                <Post message={postData[0].message} likeCount={postData[0].likeCount}/>
+                <Post message={postData[1].message} likeCount={postData[1].likeCount}/>
                 {/* вызываем функцию дважды с атрибутом message и благодаря props из Post.jsx данные подставляются в функцию */}
             </div>
         </div>
